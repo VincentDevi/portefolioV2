a9200cb105ecb96e9b255c47a06f969b9ba904e1
Devilers Vincent <vincent.devilers@gmail.com>
2022-11-21 11:35:26 +0100

    deleted weather app and added whack a mole

--- modules/gsapAnimaton.js
+++
@@ -1,9 +0,0 @@
-import {gsap} from 'gsap';
-export const swipe = (anchor) =>{
-    console.log('ok');
-    gsap.to(".mainContent",{duration: 2, scrollTo:anchor});
-}
-
-export const swipeUp = () =>{
-    gsap.to(".mainContent", {duration: 1, translateY: '50%'});
-}
